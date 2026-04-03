e429d8aa9a92da2b3f73567bfff079ecdbe752f0
makmur45 <31188096+thomaskeren90@users.noreply.github.com>
2026-04-03 12:42:04 +0700

    fix: update custom_coa module to Odoo 13

--- addons/custom_coa/__manifest__.py
+++ addons/custom_coa/__manifest__.py
@@ -1,8 +1,8 @@
 {
     "name": "Custom Chart of Accounts - Indonesia",
-    "version": "16.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Accounting",
-    "summary": "5 Banks, Expenses (Toko/Pribadi/Kos), Sales, Purchase, Inventory",
+    "summary": "5 Banks (BCA x3, SAQU, Superbank), Expenses, Sales, Purchase, Inventory",
     "author": "Custom",
     "license": "LGPL-3",
     "depends": ["account", "sale", "purchase", "stock"],
