3ea4ce1987436e98dbcde4700e70343c0123b41d
makmur45 <31188096+thomaskeren90@users.noreply.github.com>
2026-04-04 19:25:26 +0700

    Update manifest: 5 banks -> 6 banks (added SeaBank)

--- addons/custom_coa/__manifest__.py
+++ addons/custom_coa/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "Custom Chart of Accounts - Indonesia",
     "version": "13.0.1.0.0",
     "category": "Accounting",
-    "summary": "5 Banks (BCA x3, SAQU, Superbank), Expenses, Sales, Purchase, Inventory",
+    "summary": "6 Banks (BCA x3, SAQU, Superbank, SeaBank), Expenses, Sales, Purchase, Inventory",
     "author": "Custom",
     "license": "LGPL-3",
     "depends": ["account", "sale", "purchase", "stock"],
