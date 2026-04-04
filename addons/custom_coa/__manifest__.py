742d2d1e3b2b10ee638f47873f588ecb445e3d81
makmur45 <31188096+thomaskeren90@users.noreply.github.com>
2026-04-04 19:36:52 +0700

    Bump to v13.0.2.0.0 - Indonesian standard COA rebuild

--- addons/custom_coa/__manifest__.py
+++ addons/custom_coa/__manifest__.py
@@ -1,15 +1,15 @@
 {
     "name": "Custom Chart of Accounts - Indonesia",
-    "version": "13.0.1.0.0",
+    "version": "13.0.2.0.0",
     "category": "Accounting",
-    "summary": "6 Banks (BCA x3, SAQU, Superbank, SeaBank), Expenses, Sales, Purchase, Inventory",
+    "summary": "Indonesian Standard COA: 6 Banks, AR/AP, Inventory, Fixed Assets, E-commerce, Rental, Kos",
     "author": "Custom",
     "license": "LGPL-3",
     "depends": ["account", "sale", "purchase", "stock"],
     "data": [
         "data/chart_of_accounts.xml",
-        "data/journals.xml",
+        "data/journals.xml"
     ],
-    "installable": True,
-    "auto_install": False,
+    "installable": true,
+    "auto_install": false
 }
